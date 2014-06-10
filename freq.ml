56846c6b9bf2aa3becfc0a499049aba9f093951a
Andre Ramaciotti da Silva <andre@ramaciotti.com>
2014-06-10 18:50:11 -0300

    Refactoring Counter into a separate module

--- freq.ml
+++ freq.ml
@@ -3,17 +3,11 @@ open Core.Std
 (* This program reads from stdin and outputs the 10 most common lines *)
 
 let build_counts () =
-  In_channel.fold_lines stdin ~init:[] ~f:(fun counts line ->
-    let count =
-      match List.Assoc.find counts line with
-      | None -> 0
-      | Some x -> x
-    in
-    List.Assoc.add counts line (count + 1)
-  )
+  In_channel.fold_lines stdin ~init:Counter.empty ~f:Counter.touch
 
 let () =
   build_counts ()
+  |> Counter.to_list
   |> List.sort ~cmp:(fun (_,x) (_,y) -> Int.descending x y)
   |> (fun l -> List.take l 10)
   |> List.iter ~f:(fun (line, count) -> printf "%3d: %s\n" count line)
