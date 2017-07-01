dc4ccb809c3895fdecbcb6489ce791d280edeaf5
Andre Ramaciotti <andre@ramaciotti.com>
2017-07-01 15:25:23 -0300

    Angular 4.2: complete sixth chapter

--- angular4.2/my-app/src/app/app.module.ts
+++ angular4.2/my-app/src/app/app.module.ts
@@ -2,19 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
